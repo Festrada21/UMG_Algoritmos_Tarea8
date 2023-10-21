631774684c56dbf756062ee137f3beee00f1cc04
Francisco Estrada <52686336+Festrada21@users.noreply.github.com>
2023-10-21 02:44:11 +0000

    se crean los primeros archivos

---
+++ cliente.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "persona.cpp"
+using namespace std;
+
+class Cliente: Persona{
+    //atributos
+    private: string nit;
+
+    //constructos
+    public:
+    Cliente(){        
+    }
+    Cliente(string nom, string ape, string dir, int tel, string n){
+
+    }
+}
--- persona.cpp
+++ persona.cpp
@@ -7,7 +7,11 @@ protected: string nombres, apellidos, direccion;
 protected:
       Persona(){
       }
-      Persona(){
+      Persona(string nom, string ape, string dir, int tel){
+            nombres= nom;
+            apellidos= ape;
+            direccion= dir;
+            telefono= tel;
       }
 
 //metodo
