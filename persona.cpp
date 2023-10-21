e3f725d3ea5d7f42f19e9688ab613046901ead31
Francisco Estrada <52686336+Festrada21@users.noreply.github.com>
2023-10-20 22:39:06 -0600

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 class Persona{
-//atributo
-protected: string nombres, apellidos, direccion;
-          int telefono;
-protected:
-      Persona(){
-      }
-      Persona(string nom, string ape, string dir, int tel){
-            nombres= nom;
-            apellidos= ape;
-            direccion= dir;
-            telefono= tel;
-      }
-
-//metodo
-void mostrar();
+	// atributos
+	protected : string nombres,apellidos,direccion;
+				int telefono;
+	// costructor
+	protected : 
+			Persona(){
+			}
+			Persona(string nom,string ape,string dir,int tel){
+				nombres = nom;
+				apellidos = ape;
+				direccion = dir;
+				telefono = tel;
+			}
+	
+	//metodo
+	void mostrar();
+	
 };
