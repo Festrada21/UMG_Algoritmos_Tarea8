5537ea77a9711a8976d801950beff53c99b594c7
Francisco Estrada <52686336+Festrada21@users.noreply.github.com>
2023-10-20 20:35:42 -0600

    Create persona.cpp
    codificacion inicial

---
+++ persona.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class Persona{
+//atributo
+protected: string nombres, apellidos, direccion;
+          int telefono;
+protected:
+      Persona(){
+      }
+      Persona(){
+      }
+
+//metodo
+void mostrar();
+};
